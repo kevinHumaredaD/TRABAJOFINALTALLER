d8b16d5b2ca80488076abc7cc84f4c77e658cfb6
CristianJhairT <cristianjhair2000@gmail.com>
2021-07-04 23:35:58 -0500

    favicon

--- visualizar_receta.php
+++ visualizar_receta.php
@@ -19,7 +19,8 @@
     <link rel="stylesheet" href="hojaestilo/estilodepagina.css">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Visualizar receta</title>
+    <link rel="shortcut icon" href="imagen/favicon.png">
 </head>
 <body>
     <?php include 'partes/header.php' ?>
